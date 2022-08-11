d1a6678dbc9ea7c26cd75b40c363d9b634aaa504
嘉嘉 <898247676@qq.com>
2022-08-11 19:22:52 +0800

    Be able to get location and long description

--- get_and_save.py
+++ get_and_save.py
@@ -20,14 +20,23 @@ soup = BeautifulSoup(re.text, 'lxml')
 tittle = soup.find('meta', property='og:title').attrs['content']
 image_url = soup.find('meta', property='og:image').attrs['content']
 description = soup.find('meta', property='og:description').attrs['content']
+location = soup.find('a', class_='title').text
+long_url = soup.find('a', class_='title').attrs['href']
+
 image_re = get(image_url)
 with open(file_name, 'wb') as f:
     f.write(image_re.content)
+
+long_re = get(BING_URL + long_url, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.63 Safari/537.36'}
+)
+long_soup = BeautifulSoup(long_re.text.replace('<br>', '\n'), 'lxml')
+long_description = long_soup.find('div', class_='ency_desc').text
+
 zd = []
 if exists(SAVE_JSON_PATH):
     with open(SAVE_JSON_PATH, 'r', encoding='UTF-8') as f:
         zd = loads(f.read())
-
-zd.append({'tittle': tittle, 'description': description, 'file_name': file_name})
+zd.append({'tittle': tittle, 'location': location, 'description': description, 'long_description': long_description, 'file_name': file_name})
+print(zd)
 with open(SAVE_JSON_PATH, 'w', encoding='UTF-8') as f:
     f.write(dumps(zd))
