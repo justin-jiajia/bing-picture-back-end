47618e5d5cfe0198fba1ef03e53faece5fc83468
justin-jiajia <898247676@qq.com>
2023-01-19 12:17:42 +0800

    Fix the problem that long_description repeat twice

---
+++ fix.py
@@ -0,0 +1,25 @@
+from json import loads, dumps
+
+with open("conf.json") as f:
+    j = loads(f.read())
+    SAVE_JSON_PATH = j["SAVE_JSON_PATH"]
+
+with open(SAVE_JSON_PATH, "r", encoding="UTF-8") as f:
+    d = loads(f.read())
+for i in range(0, len(d)):
+    d[i]["long_description"] = d[i]["long_description"].replace(" ", "")
+    if "阅读更多" in d[i]["long_description"]:
+        d[i]["long_description"] = d[i]["long_description"][
+            d[i]["long_description"].index("阅读更多") + 4 :
+        ]
+        pass
+    if (
+        len(d[i]["long_description"]) % 2 == 0
+        and d[i]["long_description"][0 : int(len(d[i]["long_description"]) / 2)]
+        == d[i]["long_description"][int(len(d[i]["long_description"]) / 2) :]
+    ):
+        d[i]["long_description"] = d[i]["long_description"][
+            : int(len(d[i]["long_description"]) / 2)
+        ]
+with open(SAVE_JSON_PATH, "w", encoding="UTF-8") as f:
+    f.write(dumps(d))
